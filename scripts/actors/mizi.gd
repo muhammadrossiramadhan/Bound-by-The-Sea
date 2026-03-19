fbb00e2ff01e3a3fcea2d4be6cb17dfb2a980218
chokorimu <rezondavar@gmail.com>
2026-03-19 20:00:29 +0700

    follow camera

--- scripts/actors/mizi.gd
+++ scripts/actors/mizi.gd
@@ -1,34 +1,33 @@
 extends CharacterBody2D
 
-@export var mizi_speed : float = 500
-# It's better to get the direction inside the process loop
+const FPS_MULTIPLIER : float = 0.8
+
+@export var mizi_speed : float = 500.0
 var mizi_direction : Vector2
+var last_anim : String = "front_idle"
 
 func _physics_process(_delta: float) -> void:
-	# 1. Get the input direction as a single vector (normalized)
+	%AnimatedSprite2D.speed_scale = FPS_MULTIPLIER
 	mizi_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	# 2. Handle Sprite Flipping (Horizontal)
 	if mizi_direction.x < 0: 
 		%AnimatedSprite2D.flip_h = false
 	elif mizi_direction.x > 0: 
 		%AnimatedSprite2D.flip_h = true
 	
-	# 3. Handle Animations (Logic for Up/Down vs Side)
-	if mizi_direction.x != 0:
-		%AnimatedSprite2D.play("side")
-	elif mizi_direction.y > 0:
-		%AnimatedSprite2D.play("running")
-	elif mizi_direction.y < 0:
-		%AnimatedSprite2D.play("back")
-	else:
-		%AnimatedSprite2D.play("idle")
-
-	# 4. Movement Logic
 	if mizi_direction != Vector2.ZERO:
 		velocity = mizi_direction * mizi_speed
+		if mizi_direction.x != 0:
+			%AnimatedSprite2D.play("side_walk")
+			last_anim = "side_idle"
+		elif mizi_direction.y > 0:
+			%AnimatedSprite2D.play("front_walk")
+			last_anim = "front_idle"
+		elif mizi_direction.y < 0:
+			%AnimatedSprite2D.play("back_walk")
+			last_anim = "back_idle"
 	else:
-		# Smoothly stop when no keys are pressed
-		velocity = velocity.move_toward(Vector2.ZERO,50)
+		velocity = velocity.move_toward(Vector2.ZERO, 40)
+		%AnimatedSprite2D.play(last_anim)
 			
 	move_and_slide()
